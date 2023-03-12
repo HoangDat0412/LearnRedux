a7a8ae4105ae262ab7c4ad1535645cbe8c6aabc7
HoangDat0412 <hoang2811dat@gmail.com>
2023-03-12 15:34:48 +0700

    đẩy code lên

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import ExerciseCarStore from './Props/ExerciseCarStore/ExerciseCarStore'
+import DemoProps from './Props/DemoProps'
+import DanhSachSanPham from './Props/DanhSachSanPham'
+import ExerciseCart from './Props/ExerciseCart/ExerciseCart'
+import BaiTapGioHangRedux from './BaiTapGioHangRedux/BaiTapGioHangRedux'
+import BaiTapGameXucXac from './BaiTapRedux/BaiTapGameXucXac'
+import BaiTapOanTuXi from './BaiTapRedux/BaiTapOanTuXi/BaiTapOanTuXi'
+import BaiTapBookingTicket from './BaiTapRedux/BaiTapBookingTicket/BaiTapBookingTicket'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        {/* <ExerciseCarStore /> */}
+        {/* <DemoProps /> */}
+        {/* <ExerciseCart /> */}
+        {/* <BaiTapGioHangRedux /> */}
+        <BaiTapGameXucXac />
+        {/* <BaiTapOanTuXi /> */}
+        {/* <BaiTapBookingTicket /> */}
+      </div>
+
+    )
+  }
 }
-
-export default App;
---
+++ src/BaiTapGioHangRedux/GioHangRedux.js
@@ -0,0 +1,127 @@
+import React, { Component } from "react";
+
+//Sử dụng thư viên connect để lấy dữ liệu từ store về
+import { connect } from "react-redux";
+
+class GioHangRedux extends Component {
+  render() {
+    console.log(this.props.gioHang);
+
+    return (
+      <div
+        className="modal fade"
+        id="modelId"
+        tabIndex={-1}
+        role="dialog"
+        aria-labelledby="modelTitleId"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog" role="document">
+          <div className="modal-content" style={{ minWidth: 800 }}>
+            <div className="modal-header">
+              <h5 className="modal-title">Giỏ hàng</h5>
+              <button
+                type="button"
+                className="close"
+                data-dismiss="modal"
+                aria-label="Close"
+              >
+                <span aria-hidden="true">×</span>
+              </button>
+            </div>
+            <div className="modal-body">
+              <table class="table">
+                <thead>
+                  <tr>
+                    <th>Mã sp</th>
+                    <th>Hình ảnh</th>
+                    <th>Tên sản phẩm</th>
+                    <th>Giá</th>
+                    <th>Số lượng</th>
+                    <th>Thành tiền</th>
+                    <th></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {this.props.gioHang.map((spGH, index) => {
+                    return (
+                      <tr key={index}>
+                        <td>{spGH.maSP}</td>
+                        <td>
+                          <img
+                            src={spGH.hinhAnh}
+                            alt={spGH.hinhAnh}
+                            width={50}
+                            height={50}
+                          />
+                        </td>
+                        <td>{spGH.tenSP}</td>
+                        <td>{spGH.gia.toLocaleString()}</td>
+                        <td>
+                          <button onClick={() => this.props.thayDoiSoLuongSP(spGH.maSP,1)}>+</button>
+                          {spGH.soLuong.toLocaleString()}
+                          <button onClick={() => this.props.thayDoiSoLuongSP(spGH.maSP,-1)}>-</button>
+                        </td>
+                        <td>{(spGH.soLuong * spGH.gia).toLocaleString()}</td>
+                        <td>
+                          {" "}
+                          <button
+                            onClick={() => this.props.xoaSanPham(spGH.maSP)}
+                          >
+                            Xoa
+                          </button>{" "}
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-dismiss="modal"
+              >
+                Close
+              </button>
+              <button type="button" className="btn btn-primary">
+                Save
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+//Hàm lấy state redux biến đổi thành props của component
+const mapStateToProps = (state) => {
+  //state là state tổng của ứng dụng chứa các state con (rootReducer)
+  return {
+    gioHang: state.stateGioHang.gioHang,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    xoaSanPham: (maSP) => {
+      let action = {
+        type: "XOA_SAN_PHAM",
+        maSP,
+      };
+      dispatch(action);
+    },
+    thayDoiSoLuongSP :(maSP,number) =>{
+        let action = {
+            type :"DOI_SO_LUONG_SP",
+            maSP,
+            number
+        }
+        dispatch(action)
+    }
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(GioHangRedux);
